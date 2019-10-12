5c4012cc4a171d5fe144cf55f61f458784dbbd18
evanpc <scram@ymail.com>
2019-10-12 14:25:06 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+version = '0.1'
+setup(
+    name='avel',
+    version='0.1',
+    description='Python ffmpeg wrapper for audio and video editing (trim, subtitles/overlay, concat, merge, & more!)',
+    license='MIT',
+    author='evliang',
+    packages=['avel'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
