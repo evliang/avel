226ed3f7b361b1d9c83718622fa16d64428e3a6d
evanpc <scram@ymail.com>
2019-10-13 14:35:42 -0700

    using call_ffmpeg function

--- avel/audio_lib.py
+++ avel/audio_lib.py
@@ -2,7 +2,7 @@ import os
 import uuid
 import shutil
 import subprocess
-from shared_lib import get_duration
+from avel.shared_lib import call_ffmpeg, get_duration
 
 def _pad_integer(i):
     return str(i).zfill(2)
@@ -18,17 +18,13 @@ def combine_audio(audio_list, output_path, transition_time=13, debugging=False):
             return temp1
     if len(audio_list) > 2:
         print(audio_list)
-        subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-y', '-i', audio_list[0], '-i', audio_list[1], '-vn',
-                f'-filter_complex', 'acrossfade=d={transition_time}:c1=tri:c2=squ', temp1])
+        call_ffmpeg(f'ffmpeg -i {audio_list[0]} -i {audio_list[1]} -vn -filter_complex acrossfade=d={transition_time}:c1=tri:c2=squ {temp1}')
         for i in range(2, len(audio_list) - 1):
-            subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-y', '-i', temp_file(i-1), '-i', audio_list[i], '-vn',
-                f'-filter_complex', 'acrossfade=d={transition_time}:c1=tri:c2=squ', temp_file(i)])
+            call_ffmpeg(f'ffmpeg -i {temp_file(i-1)} -i {audio_list[i]} -vn -filter_complex acrossfade=d={transition_time}:c1=tri:c2=squ {temp_file(i)}')
         # final call to convert to mp3
-        subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-y', '-i', temp_file(len(audio_list) - 2), '-i', audio_list[-1], '-vn',
-            f'-filter_complex', 'acrossfade=d={transition_time}:c1=tri:c2=squ', output_path])
+        call_ffmpeg(f'ffmpeg -i {temp_file(len(audio_list) - 2)} -i {audio_list[-1]} -vn -filter_complex acrossfade=d={transition_time}:c1=tri:c2=squ {output_path}')
     elif len(audio_list) == 2:
-        subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-y', '-i', audio_list[0], '-i', audio_list[1], '-vn',
-            f'-filter_complex', 'acrossfade=d={transition_time}:c1=tri:c2=squ', output_path])
+        call_ffmpeg(f'ffmpeg -i {audio_list[0]} -i {audio_list[1]} -vn -filter_complex acrossfade=d={transition_time}:c1=tri:c2=squ {output_path}')
     elif len(audio_list) == 1:
         shutil.copyfile(audio_list[0], output_path)
     else:
@@ -45,13 +41,9 @@ def combine_audio(audio_list, output_path, transition_time=13, debugging=False):
 
 def extract_audio(video_file, time1, time2, output_file):
     """Creates audio file from video and timestamps"""
-    subprocess.call(['ffmpeg', '-y', #'-hide_banner', '-loglevel', 'panic',
-                    '-i', video_file,
-                    '-ss', time1, '-to', time2, #f"volume=enable='between(t,t1,t2)':volume=0, volume=enable='between(t,t3,t4)':volume=0",
-                    '-c:a', 'libmp3lame', output_file])
+    call_ffmpeg(f'ffmpeg -i {video_file} -ss {time1} -to {time2} -c:a libmp3lame {output_file}')
+     #f"volume=enable='between(t,t1,t2)':volume=0, volume=enable='between(t,t3,t4)':volume=0",
 
 def merge_audio(audio_file1, audio_file2, output_file, vol1=1.0, vol2=1.0):
     """Merges two audios into one. option to adjust volumes for both audio"""
-    subprocess.call(['ffmpeg', '-y', #'-hide_banner', '-loglevel', 'panic',
-                    '-i', audio_file1, '-i', audio_file2, '-filter_complex',
-                    f'[0:0]volume={vol1}[a];[1:0]volume={vol2}[b];[a][b]amix=inputs=2:duration=longest', '-c:a', 'libmp3lame', output_file])
+    call_ffmpeg(f'ffmpeg -i {audio_file1} -i {audio_file2} -filter_complex [0:0]volume={vol1}[a];[1:0]volume={vol2}[b];[a][b]amix=inputs=2:duration=longest -c:a libmp3lame {output_file}')
--- avel/video_lib.py
+++ avel/video_lib.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import subprocess
-from shared_lib import call_external, get_duration
+from avel.shared_lib import call_ffmpeg, call_external, get_duration
 
 def get_dimensions(path):
     """Get dimensions of a video"""
@@ -20,12 +20,11 @@ def _get_framerate(path):
 
 def trim_video(inp, start_time, end_time, outp):
     """Trims a single video"""
-    subprocess.call(['ffmpeg', '-y', 
-                    '-i', inp, '-ss', start_time, '-to', end_time, outp])
+    call_ffmpeg(f'ffmpeg -i {inp} -ss {start_time} -to {end_time} {outp}')
 
 def create_image_video(image, seconds, output_filename):
     """creates video from single image. seconds determines length of video"""
-    subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-loop', '1', '-i', image, '-c:v', 'libx264', '-t', str(seconds), '-pix_fmt', 'yuv420p', '-vf', 'pad=ceil(iw/2)*2:ceil(ih/2)*2', output_filename])
+    call_ffmpeg(f'ffmpeg -loop 1 -i {image} -c:v libx264 -t {seconds} -pix_fmt yuv420p -vf pad=ceil(iw/2)*2:ceil(ih/2)*2 {output_filename}')
 
 # https://github.com/tanersener/ffmpeg-video-slideshow-scripts/tree/master/transition_scripts
 def create_image_slideshow_fade(images, seconds, output_filename):
@@ -49,7 +48,7 @@ def create_image_slideshow_fade(images, seconds, output_filename):
         create_image_video(images[0], seconds, output_filename)
     else:
         raise(ValueError("No images for slideshow"))
-    subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-y'] + part1 + ['-filter_complex'] + [part2 + part3] + ['-map', '[v]', output_filename])
+    call_ffmpeg(f'ffmpeg {part1} -filter_complex {part2 + part3} -map [v] {output_filename}')
 
 def create_scrolling_image(image, seconds, direction, output_filename):
     """Creates horizontal video to scroll through a vertical image."""
@@ -59,28 +58,34 @@ def create_scrolling_image(image, seconds, direction, output_filename):
     speed = int((height - adj_height * 1.0)/seconds)
 
     if direction == 'zoomout':
-        subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-loop', '1', '-t', str(seconds), '-i', image, '-vf', '''"zoompan=z='if(lte(zoom,1.0),1.5,max(1.001,zoom-0.0015))':d=125"''', '-c:v', 'libx264', output_filename,  '-y'])
+        # subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-loop', '1', '-t', str(seconds), '-i', image, '-vf', '''"zoompan=z='if(lte(zoom,1.0),1.5,max(1.001,zoom-0.0015))':d=125"''', '-c:v', 'libx264', output_filename,  '-y'])
+        call_ffmpeg(f'''ffmpeg -loop 1 -t {seconds} -i {image} -vf "zoompan=z='if(lte(zoom,1.0),1.5,max(1.001,zoom-0.0015))':d=125" -c:v libx264 {output_filename}''')
     elif direction == 'zoomin':
         # subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-r', '25', '-i', image, '-filter_complex', "scale=-2:10*ih,zoompan=z='min(zoom+0.0015,1.5)':d=125:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)',scale=-2:720", '-shortest', '-c:v', 'libx264', output_filename,  '-y'])
         (width, height) = get_dimensions(image)
         width = int(width * 0.25)
         height = int(height * 0.25)
-        subprocess.call(['ffmpeg', '-loop', '1', '-i', image, '-vf', f"zoompan=z='zoom+0.001:s={width}x{height}", '-c:v', 'libx264', '-preset', 'fast', '-t', str(seconds), '-y', output_filename ])
+        call_ffmpeg(f'''ffmpeg -loop 1 -t {seconds} -i {image} -vf zoompan=z='zoom+0.001:s={width}x{height} -c:v libx264 -preset fast {output_filename}''')
+        #subprocess.call(['ffmpeg', '-loop', '1', '-i', image, '-vf', f"zoompan=z='zoom+0.001:s={width}x{height}", '-c:v', 'libx264', '-preset', 'fast', '-t', str(seconds), '-y', output_filename ])
     elif direction == "down":
-        subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-loop', '1', '-t', str(seconds), '-i', image, '-filter_complex', 'color=white:s=' + dimensions + "[bg];[bg][0]overlay=y=-'t*" + str(speed) + "':shortest=1[video]", '-r', '25/1', '-preset', 'ultrafast', '-map', '[video]', output_filename,  '-y'])
+        call_ffmpeg(f'''ffmpeg -loop 1 -t {seconds} -i {image} -filter_complex color=white:s={dimensions}[bg];[bg][0]overlay=y=-'t*{speed}':shortest=1[video] -r 25/1 -preset ultrafast -map [video] {output_filename}''')
+        #subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-loop', '1', '-t', str(seconds), '-i', image, '-filter_complex', 'color=white:s=' + dimensions + "[bg];[bg][0]overlay=y=-'t*" + str(speed) + "':shortest=1[video]", '-r', '25/1', '-preset', 'ultrafast', '-map', '[video]', output_filename,  '-y'])
     else:
         # going up:
-        subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-loop', '1', '-t', str(seconds), '-i', image, '-filter_complex', 'color=white:s=' + dimensions + "[bg];[bg][0]overlay=y=main_h-overlay_h+'t*" + str(speed) + "':shortest=1[video]", '-r', '25/1', '-preset', 'ultrafast', '-map', '[video]', output_filename, '-y'])
+        call_ffmpeg(f'''ffmpeg -loop 1 -t {seconds} -i {image} -filter_complex color=white:s={dimensions}[bg];[bg][0]overlay=y=main_h-overlay_h+'t*{speed}':shortest=1[video] -r 25/1 -preset ultrafast -map [video] {output_filename}''')
+        #subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-loop', '1', '-t', str(seconds), '-i', image, '-filter_complex', 'color=white:s=' + dimensions + "[bg];[bg][0]overlay=y=main_h-overlay_h+'t*" + str(speed) + "':shortest=1[video]", '-r', '25/1', '-preset', 'ultrafast', '-map', '[video]', output_filename, '-y'])
 
 def slowmo(input_vid, output_path, multiplier=2.0, duration=None):
     if duration:
         video_dur = get_duration(input_vid)
         if duration > video_dur:
-            subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-i', input_vid, '-filter:v', 'setpts=' + str(duration*1.0/video_dur) + '*PTS', output_path, '-y'])
+            call_ffmpeg(f'ffmpeg -i {input_vid} -filter:v setpts= {duration*1.0/video_dur} *PTS {output_path}')
+            #subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-i', input_vid, '-filter:v', 'setpts=' + str(duration*1.0/video_dur) + '*PTS', output_path, '-y'])
         else:
             shutil.copyfile(input_vid, output_path)
     else:
-        subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-i', input_vid, '-filter:v', 'setpts=' + str(multiplier) + '*PTS', output_path, '-y'])
+        call_ffmpeg(f'ffmpeg -i {input_vid} -filter:v setpts= {multiplier} *PTS {output_path}')
+        #subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-i', input_vid, '-filter:v', 'setpts=' + str(multiplier) + '*PTS', output_path, '-y'])
 
 def combine_videos(files, output_filename, debugging=False):
     """Concatting video files of all dimensions and durations together"""
@@ -89,8 +94,9 @@ def combine_videos(files, output_filename, debugging=False):
 
     if (len(files) < 400):
         for (idx, f) in enumerate(files):
-            subprocess.call(['ffmpeg', '-y', '-i', f, '-c', 'copy', '-bsf:v', 'h264_mp4toannexb', '-f', 'mpegts', intermediate_file(idx)])
-        subprocess.call(['ffmpeg', '-y', '-i', 'concat:' + '|'.join(map(lambda x: intermediate_file(x), range(0,len(files)) )), '-c', 'copy', output_filename])
+            call_ffmpeg(f'ffmpeg -i {f} -c copy -bsf:v h264_mp4toannexb -f mpegts {intermediate_file(idx)}')
+        call_ffmpeg(f"ffmpeg -i concat:{'|'.join(map(lambda x: intermediate_file(x), range(0,len(files)) ))} -c copy {output_filename}")
+        #subprocess.call(['ffmpeg', '-y', '-i', 'concat:' + '|'.join(map(lambda x: intermediate_file(x), range(0,len(files)) )), '-c', 'copy', output_filename])
 
         if not debugging:
             for (idx, _) in enumerate(files):
@@ -101,24 +107,19 @@ def combine_videos(files, output_filename, debugging=False):
             f.write('\n'.join(map(lambda x: "file '" + x + "'", files)))
         # this seems to work as well, but I get a lot of warning-like messages:
         # os.system('ffmpeg -hide_banner -loglevel panic -f concat -safe 0 -i templist.txt -codec copy ' + output_filename)
-        subprocess.call(['ffmpeg',
-                '-hide_banner', '-loglevel', 'panic', 
-                '-f', 'concat', '-safe', '0',
-                '-i', 'templist.txt', '-codec', 'copy', output_filename])
+        call_ffmpeg(f'ffmpeg -i templist.txt -f concat -safe 0 -codec copy {output_filename}')
 
 def blur_video(filePath, output_filename, resolution=None, debugging=False):
     """Creates a video with blurred sides (for clips that are not the same width as the final video's width.
     This option works well for now, but takes the resolution from the first video"""
-    subprocess.call(['ffmpeg', '-y', '-i', filePath,
-        '-filter_complex', r"[0:v]scale=ih*16/9:-1,boxblur=luma_radius=min(h\,w)/20:luma_power=1:chroma_radius=min(cw\,ch)/20:chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop='if(gte(dar,16/9),ih*16/9,iw)':'if(gte(dar,16/9),ih,iw*9/16)'",
-        output_filename])
+    call_ffmpeg(f'ffmpeg -i {filePath} -filter_complex ' + r"[0:v]scale=ih*16/9:-1,boxblur=luma_radius=min(h\,w)/20:luma_power=1:chroma_radius=min(cw\,ch)/20:chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop='if(gte(dar,16/9),ih*16/9,iw)':'if(gte(dar,16/9),ih,iw*9/16)' " + output_filename)
     if not debugging:
         os.remove(filePath)
     return output_filename
 
 def combine_audio_video(audioPath, videoPath, output_filename, debugging=False):
     """Combining audio and video into one video file. Shorter duration of video/audio determines the final video's duration"""
-    subprocess.call(['ffmpeg', '-y', '-hide_banner', '-loglevel', 'panic', '-i', audioPath, '-i', videoPath, '-codec', 'copy', '-shortest', output_filename])
+    call_ffmpeg(f'ffmpeg -i {audioPath} -i {videoPath} -codec copy -shortest {output_filename}')
 
     if not debugging:
         os.remove(audioPath)
